7acae1809455996c6b122d8372ed9b71db3685ca
simonkey1 <cristobalc.gomez@mail.udp.cl>
2024-12-30 14:02:01 -0300

    Añadir gráfico de tendencias y modularidad con charts.py

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import streamlit as st
 import scraping  # Importa el archivo de scraping como módulo
 
 # Título de la Landing Page
-st.title("Mi Portfolio de Proyectos")
+st.title("Simón Gómez | Sociólogo y Data Scientist")
+st.subheader("Mi Portfolio de Proyectos")
+st.write("¡Hola!, me apasiona la ciencia de datos y la programación.")
 st.markdown("Bienvenido a mi portfolio. Explora mis proyectos y habilidades.")
+st.button("Currículum Vitae")
+st.button("Contactar")
+st.button("GitHub")
+st.button("LinkedIn")
+st.button("Proyectos")
 
 # Menú de navegación
 menu = st.sidebar.selectbox(
